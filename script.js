72f786afbaf2d6f8071860d59954a25bf320fb37
Danyal Sheikh <60336684+humanebean@users.noreply.github.com>
2024-04-30 01:09:28 -0500

    added video background, calls to API, form submission. Now user can enter location and temp in F is displayed for current day and next 2 days.

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function createDivWithText(text) {
+    let newDiv = document.createElement('div');
+      newDiv.textContent = text;
+      weatherStuff.appendChild(newDiv);
+  }
+  
+
+async function getWeatherData(target){
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=f2fecc8a07584e8c9e8224802240304&q='+target+'&days=3&aqi=no&alerts=no', {mode: 'cors'});
+    const weatherData = await response.json()
+    return weatherData
+    console.log(weatherData.location)
+}
+weatherStuff = document.getElementById("weatherStuff")
+weatherForm =document.getElementById("weatherForm")
+weatherForm.addEventListener('click',async (event)=>{
+    event.preventDefault();
+    formInput = document.getElementById("weatherTarget").value;
+    let weatherData = await getWeatherData(formInput);
+    console.log(weatherData)
+    createDivWithText(weatherData.location.name)
+    createDivWithText(weatherData.forecast.forecastday[0].date)
+    createDivWithText(weatherData.forecast.forecastday[0].day.avgtemp_f)
+    createDivWithText(weatherData.forecast.forecastday[1].date)
+    createDivWithText(weatherData.forecast.forecastday[1].day.avgtemp_f)
+    createDivWithText(weatherData.forecast.forecastday[2].date)
+    createDivWithText(weatherData.forecast.forecastday[2].day.avgtemp_f)
+})
