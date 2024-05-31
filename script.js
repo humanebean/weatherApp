55ad1af659fbc459ae8aa0dbf53bba68983dec16
Danyal Sheikh <60336684+humanebean@users.noreply.github.com>
2024-05-30 19:03:09 -0500

    added background video

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 
-function createDivWithText(text) {
+function createDivWithText(text,cls) {
     let newDiv = document.createElement('div');
       newDiv.textContent = text;
+      newDiv.classList.add(cls);
       weatherStuff.appendChild(newDiv);
   }
   function createDivWithImg(src) {
@@ -18,24 +19,26 @@ async function getWeatherData(target){
     return weatherData
     console.log(weatherData.location)
 }
-weatherStuff = document.getElementById("weatherStuff")
-weatherForm =document.getElementById("weatherForm")
-weatherForm.addEventListener('click',async (event)=>{
+weatherStuff = document.getElementById("weatherStuff");
+weatherForm =document.getElementById("weatherForm");
+submitButton=document.getElementById("submitButton");
+submitButton.addEventListener('click',async (event)=>{
     event.preventDefault();
+    weatherStuff.innerHTML="";
     formInput = document.getElementById("weatherTarget").value;
     let weatherData = await getWeatherData(formInput);
     console.log(weatherData)
-    createDivWithText(weatherData.location.name)
-    createDivWithText(weatherData.forecast.forecastday[0].date)
-    createDivWithText(weatherData.forecast.forecastday[0].day.avgtemp_f)
-    createDivWithText(weatherData.forecast.forecastday[0].day.condition.text)
-    createDivWithImg(weatherData.forecast.forecastday[0].day.condition.icon)
-    createDivWithText(weatherData.forecast.forecastday[1].date)
-    createDivWithText(weatherData.forecast.forecastday[1].day.avgtemp_f)
-    createDivWithText(weatherData.forecast.forecastday[1].day.condition.text)
-    createDivWithImg(weatherData.forecast.forecastday[1].day.condition.icon)
-    createDivWithText(weatherData.forecast.forecastday[2].date)
-    createDivWithText(weatherData.forecast.forecastday[2].day.avgtemp_f)
-    createDivWithText(weatherData.forecast.forecastday[2].day.condition.text)
-    createDivWithImg(weatherData.forecast.forecastday[2].day.condition.icon)
+    createDivWithText(weatherData.location.name, "zero")
+    createDivWithText(weatherData.forecast.forecastday[0].date,"zero")
+    createDivWithText(weatherData.forecast.forecastday[0].day.avgtemp_f,"zero")
+    createDivWithText(weatherData.forecast.forecastday[0].day.condition.text,"zero")
+    createDivWithImg(weatherData.forecast.forecastday[0].day.condition.icon,"zero")
+    createDivWithText(weatherData.forecast.forecastday[1].date,"one")
+    createDivWithText(weatherData.forecast.forecastday[1].day.avgtemp_f,"one")
+    createDivWithText(weatherData.forecast.forecastday[1].day.condition.text,"one")
+    createDivWithImg(weatherData.forecast.forecastday[1].day.condition.icon,"one")
+    createDivWithText(weatherData.forecast.forecastday[2].date,"two")
+    createDivWithText(weatherData.forecast.forecastday[2].day.avgtemp_f,"two")
+    createDivWithText(weatherData.forecast.forecastday[2].day.condition.text,"two")
+    createDivWithImg(weatherData.forecast.forecastday[2].day.condition.icon,"two")
 })
